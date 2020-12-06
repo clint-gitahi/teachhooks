cd97ccbd22a1e8a20c1597e18a348f48a8a175c5
clint-gitahi <clingitahi@gmail.com>
2020-12-06 12:38:36 +0300

    fetching a single post.

--- src/Datafetching.js
+++ src/Datafetching.js
@@ -3,26 +3,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState({});
+    const [id, setId] = useState(1);
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
                 console.log(res)
-                setPosts(res.data)
+                setPost(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
-    }, [])
+    }, [idFromButtonClick])
 
     return (
         <div>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+            <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <button type="button" onClick={handleClick}>Fetch Post</button>
+            <div>{post.title}</div>
+
         </div>
     )
 }

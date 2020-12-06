aad5be7537c3d01d59dc9eb2cdff1e1c37890f56
clint-gitahi <clingitahi@gmail.com>
2020-12-06 21:09:04 +0300

    using both useReducer and useContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
-import HooksCounterTwo from './hooksCounter'
-import HooksCounterThree from './hooksCounterThree'
-import SideEffect from './sideEffects1'
-import SideEffectsCleanup from './sideEffectsCleanup'
-import DataFetching from './Datafetching'
-import CountReducer from './CounterReducer'
+// import HooksCounterTwo from './hooksCounter'
+// import HooksCounterThree from './hooksCounterThree'
+// import SideEffect from './sideEffects1'
+// import SideEffectsCleanup from './sideEffectsCleanup'
+// import DataFetching from './Datafetching'
+// import CountReducer from './CounterReducer'
+// import CountReducerTwo from './CounterTwoReducer'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+
+export const CountContext = React.createContext()
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+}
 
 function App() {
+
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <CountReducer />
-    </div>
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className="App">
+
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
---
+++ src/components/ComponentA.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { CountContext } from '../App'
+
+function ComponentA() {
+    const countContext = useContext(CountContext)
+    return (
+        <div>
+            <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
+            <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
+            <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
+        </div>
+    )
+}
+
+export default ComponentA

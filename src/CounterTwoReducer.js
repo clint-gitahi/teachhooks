2f6c2f60377f4f38f4c3ee7c92f35586fdb4f01a
clint-gitahi <clingitahi@gmail.com>
2020-12-06 20:36:11 +0300

    another way for usereducer.

---
+++ src/CounterTwoReducer.js
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react';
+
+const initialState = {
+    firstCounter: 0
+};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { firstCounter: state.firstCounter + 1 };
+        case 'decrement':
+            return { firstCounter: state.firstCounter - 1 };
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+}
+
+function CountReducer() {
+    const [count, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <div>
+            <div>Count - {count.firstCounter}</div>
+            <button onClick={() => dispatch({ type: 'increment', value: 1 })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 1 })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'increment', value: 5 })}>Increment 5</button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 5 })}>Decrement 5</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+        </div>
+    )
+}
+
+export default CountReducer;

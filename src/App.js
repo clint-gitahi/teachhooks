deb1af3f59211dfeb1536e39719f824752e79f1e
clint-gitahi <clingitahi@gmail.com>
2020-12-06 12:46:50 +0300

    adding axios for data fetching.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HooksCounterTwo from './hooksCounter'
 import HooksCounterThree from './hooksCounterThree'
+import SideEffect from './sideEffects1'
+import SideEffectsCleanup from './sideEffectsCleanup'
+import DataFetching from './Datafetching'
 
 function App() {
   return (
     <div className="App">
-      <HooksCounterThree />
+      <DataFetching />
     </div>
   );
 }

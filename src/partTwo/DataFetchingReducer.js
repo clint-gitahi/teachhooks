3dbd9bf5122f8c00187bb8c98f9c4b558f83cc0b
clint-gitahi <clingitahi@gmail.com>
2020-12-07 05:03:32 +0300

    fetch data using a useReducer.

--- src/partTwo/DataFetchingReducer.js
+++ src/partTwo/DataFetchingReducer.js
@@ -1,29 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import axios from 'axios'
+
+const initialState = {
+    loading: true,
+    error: '',
+    post: {}
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            return {
+                loading: false,
+                post: action.payload,
+                error: ''
+            }
+        case 'FETCH_ERROR':
+            return {
+                loading: false,
+                post: {},
+                error: 'Something went wrong!'
+            }
+        default:
+            return state;
+    }
+}
 
 function DataFetchingReducer() {
 
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('')
-    const [post, setPost] = useState({})
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts/1')
             .then(response => {
-                setLoading(false)
-                setPost(response.data)
-                setError('')
+                dispatch({
+                    type: 'FETCH_SUCCESS',
+                    payload: response.data
+                })
             })
             .catch(error => {
-                setLoading(false)
-                setPost({})
-                setError('Something went wrong..')
+                dispatch({
+                    type: 'FETCH_ERROR',
+                })
             })
     }, [])
     return (
         <div>
-            {loading ? 'Loading' : post.title}
-            {error ? error : null}
+            {state.loading ? 'Loading' : state.post.title}
+            {state.error ? state.error : null}
         </div>
     )
 }

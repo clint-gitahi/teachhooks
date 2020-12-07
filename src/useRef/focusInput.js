4a1c15a00b8c189fd1c3e7a3c0020ff8770a0593
clint-gitahi <clingitahi@gmail.com>
2020-12-07 05:03:56 +0300

    focus on an input using useRef

---
+++ src/useRef/focusInput.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from 'react'
+
+function FocusInput() {
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        // focus on the input element
+        inputRef.current.focus()
+    }, [])
+    return (
+        <div>
+            <input ref={inputRef} type='text' />
+        </div>
+    )
+}
+
+export default FocusInput
